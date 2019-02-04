091459f225235a916dc533136cbce8ba9ce02ad6
Rana Khalil <rana.ae.khalil@gmail.com>
2019-02-03 20:49:26 -0800

    Initial commit with localization package, all deps needed for it and make bash file

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt-get install ros-kinetic-map-server
+sudo pip install cython
+git clone http://github.com/kctess5/range_libc 
+cd range_libc/pywrapper
+./compile.sh
+cd ../../
+sudo rm -rf range_libc
